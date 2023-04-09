fb3a355f304de67b9c6979e41d1a283b7aaf4678
Abhishek Yelley <94790503+abhishekyelley@users.noreply.github.com>
2023-04-09 18:36:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
-
-apt-get update && apt-get install libgl1
+sudo apt-get install libgl1-mesa-glx
+sudo apt-get update && sudo apt-get install libgl1
 
 sudo apt-get update
 sudo apt-get install -y python3-opencv
