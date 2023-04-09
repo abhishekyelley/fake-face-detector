8c159a712a903702f57e0fc17eb9899e2329b377
Abhishek Yelley <94790503+abhishekyelley@users.noreply.github.com>
2023-04-09 18:39:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
-
 #!/bin/bash
-
 sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx libgl1
-sudo ldconfig
-
-sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx
 sudo apt-get install -y python3-opencv
-
 pip install -r requirements.txt
